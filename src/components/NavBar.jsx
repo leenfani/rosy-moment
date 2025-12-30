3a90efc1b8bff13bd0ce7432bd5668b71bdb6df7
leen fani <leenfani06@gmail.com>
2025-12-30 11:32:52 +0200

    styling the icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
+// grid
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+
+// btn
+import Button from "@mui/material/Button";
+
 // icons
 import PersonIcon from "@mui/icons-material/Person";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
-
-// grid
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import { blue } from "@mui/material/colors";
 
 export default function NavBar() {
   return (
@@ -17,21 +19,50 @@ export default function NavBar() {
           <Grid bgcolor={"black"} size={8}>
             <h1 className="text-5xl font-MonteCarlo">Rosy Moment</h1>
           </Grid>
-          <Grid container direction="row-reverse" size={4} spacing={4}>
-            <Grid>
+
+          {/* icons */}
+
+          <Grid
+            container
+            spacing={1}
+            size={4}
+            display="flex"
+            flexDirection="row-reverse"
+            justifyItems="flex-end"
+            sx={{
+              "& .MuiButton-root": {
+                borderRadius: 0,
+                color: "common.white",
+                transition: "box-shadow 0.2s ease",
+
+                "&:focus": {
+                  outline: "none",
+                },
+                "&:hover": {
+                  boxShadow: "inset 0 -2px 0 rgba(255,255,255,0.5)",
+                },
+              },
+            }}
+          >
+            <Button key="login">
               <PersonIcon fontSize="large" />
-              Login
-            </Grid>
-            <Grid>
+              login
+            </Button>
+
+            <Button key="shoppingcart">
               <ShoppingCartIcon fontSize="large" />
-            </Grid>
-            <Grid>
+            </Button>
+
+            <Button key="darkMood">
               <DarkModeIcon fontSize="large" />
-            </Grid>
-            <Grid>
+            </Button>
+
+            <Button key="lightMood">
               <LightModeIcon fontSize="large" />
-            </Grid>
+            </Button>
           </Grid>
+
+          {/* ==icons== */}
         </Grid>
       </Box>
     </nav>
