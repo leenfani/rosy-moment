c535539f6cda3e11796286810a391ab2f19d3505
leen fani <leenfani06@gmail.com>
2026-03-11 16:05:22 +0200

    refactor: migrate cart snack bar to TypeScript

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,4 @@ export * from "./authContextTypes";
 export * from "./productsTypes/flowersBouquet";
 export * from "./productsTypes/products";
 export * from "./cart"
+export * from "./cartSnackbar"
